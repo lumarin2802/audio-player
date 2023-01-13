1f1e01164842d27026e7a8be049364ff25005400
lumarin2802 <marinperezluciana@gmail.com>
2023-01-13 14:03:49 +0000

    audio player funcionando, pendiente estetica adecuada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,59 +1,103 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const Home = () => {
+const App = () => {
+
+  let [urlDir, setUrlDir] = useState("");
+  const [music, setMusic] = useState([]);
+  // let [pedazoUrl, setPedazoUrl] = useState("");
+  let [currentSong, setCurrentSong] = useState("")
+  let [isPlaying, setIsPlaying] = useState(false)
+  let audioRef = useRef()
+
  
-    let [urlDir, setUrlDir] = useState("");
-    const [music, setMusic] = useState([]);
+  const handleClick = (event, key, title) => {
+    console.log(event.target);
+    console.log(key);
+    setUrlDir("https://assets.breatheco.de/apis/sound/" + key);
+    // setPedazoUrl(key)
+    setCurrentSong(title);
+        if(isPlaying){
+      setIsPlaying(false)
+      }
+        };
 
-    const handleClick = (event, key) => {
-        console.log(event.target);
-        console.log(key);
-        setUrlDir("https://assets.breatheco.de/apis/sound/" + key);
-      };
+        
 
- console.log(urlDir)
+  useEffect(() => {
 
-    useEffect(() => {
-
-        fetch('https://assets.breatheco.de/apis/sound/songs').then((response) => response.json()).then((data) => setMusic(data))
-    }, [])
-
-    return (<div className="container">
-        <div id="interface" className="interfaceClass">
-			<div id="header" className="headerClass text-light text-center m-auto">
-				<h5 className="p-2">BreatheCode Orange Marios Bros Player</h5>
-			</div>
-            
-                
-
-<div id="listGroup" className="listClass m-4">
-                <ol className="pb-3 pt-3"> {music.map((item, key) =>< li onClick={event => handleClick(event, item.url)} className="bg-warning m-1" key = {item.url}>{item.name} </li>)} </ol>
-             </div>
+    fetch('https://assets.breatheco.de/apis/sound/songs').then((response) => response.json()).then((data) => setMusic(data))
+  }, [])
 
 
 
-            <div id="playerPrbador" className="">
-            <audio controls>
-              <source
-                src={urlDir}
-                type="audio/ogg"
-              />
-              Your browser does not support the audio tag.
-            </audio>
-          </div>
 
-<div>
-    <p></p>
-</div>
 
-            <div id="controlBar" className="m-3 d-flex justify-content-center">
-                <button className="roundButton m-1"><i className="fa fa-backward"></i></button>
-                <button className="roundButton m-1"><i className="fa fa-play"></i></button>
-                <button className="roundButton m-1"><i className="fa fa-forward"></i></button>
-            </div>
-        </div>
+
+  const playNext = () => {
+		for (let i = 0; i < music.length; i++) {
+      let a = i
+      a = a + 1
+      if ("https://assets.breatheco.de/apis/sound/"+music[i].url === urlDir) 
+      {
+        setUrlDir("https://assets.breatheco.de/apis/sound/"+music[a].url)
+        setIsPlaying(false)
+        setCurrentSong(music[a].name)
+       }
+      
+    }}
+
+    const playPrevious = () => {
+      for (let i = 0; i < music.length; i++) {
+        let a = i
+        a = a - 1
+        if ("https://assets.breatheco.de/apis/sound/"+music[i].url === urlDir) 
+        {
+          setUrlDir("https://assets.breatheco.de/apis/sound/"+music[a].url)
+          setIsPlaying(false)
+          setCurrentSong(music[a].name)
+        }
+        
+      }}
+
+  const play = () =>{
+    const audio = audioRef.current
+    audio.volume = 0.5
+    
+    if(!isPlaying){
+    setIsPlaying(true)
+    audio.play()
+    }
+    
+    if(isPlaying){
+    setIsPlaying(false)
+    audio.pause()
+    }
+    }
+
+  return (<div className="container">
+    <div id="interface" className="interfaceClass">
+      <div id="header" className="headerClass text-light text-center m-auto">
+        <h5 className="p-2">BreatheCode Orange 8bits Audio Player</h5>
+      </div>
+
+
+      <div id="listGroup" className="listClass m-4 pb-1 pt-1">
+        <div> {music.map((item, key, title) => < div onClick={event => handleClick(event, item.url, item.name)} className="claseElemento m-1 text-center" key={item.url}>{item.name} </div>)} </div>
+      </div>
+
+      <div className="text-light text-center p-1">
+        <p>Current Song: {currentSong}</p>
+                   </div>
+   
+      <div id="controlBar" className="p-1 d-flex justify-content-center">
+      <audio src={urlDir} ref={audioRef}> </audio>
+        <button onClick={playPrevious} className="roundButton m-1"><i className="fa fa-backward"></i></button>
+        <button onClick={play} className="roundButton m-1"><i className="fa fa-play"></i></button>
+        <button onClick={playNext} className="roundButton m-1"><i className="fa fa-forward"></i></button>
+      </div>
     </div>
-);;
+  </div>
+  );;
 };
 
-export default Home;
+export default App;
